7d155be7b6b69f1d92ee06b107945f7a6e46493c
JCarlosMtzZ <juancmtz777@gmail.com>
2024-11-23 12:52:56 -0600

    End of section 4

--- src/AuctionsService/Data/AuctionDbContext.cs
+++ src/AuctionsService/Data/AuctionDbContext.cs
@@ -1,4 +1,5 @@
 using AuctionsService.Entities;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 
 namespace AuctionsService.Data;
@@ -6,4 +7,14 @@ namespace AuctionsService.Data;
 public class AuctionDbContext(DbContextOptions options) : DbContext(options)
 {
     public DbSet<Auction> Auctions { get; set; }
+
+    // Outbox configuration. Creates tables in the DB
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.AddInboxStateEntity();
+        modelBuilder.AddOutboxMessageEntity();
+        modelBuilder.AddOutboxStateEntity();
+    }
 }
--- src/AuctionsService/RequestHelpers/MappingProfiles.cs
+++ src/AuctionsService/RequestHelpers/MappingProfiles.cs
@@ -1,6 +1,7 @@
 using AuctionsService.DTOs;
 using AuctionsService.Entities;
 using AutoMapper;
+using Contracts;
 
 namespace AuctionsService.RequestHelpers;
 
@@ -12,8 +13,16 @@ public class MappingProfiles : Profile
 
         CreateMap<Auction, AuctionDto>().IncludeMembers(x => x.Item);
         CreateMap<Item, AuctionDto>();
+
         CreateMap<CreateAuctionDto, Auction>()
             .ForMember(d => d.Item, o => o.MapFrom(s => s));
+
         CreateMap<CreateAuctionDto, Item>();
+        CreateMap<AuctionDto, AuctionCreated>();
+
+        CreateMap<Auction, AuctionUpdated>().IncludeMembers(a => a.Item);
+        CreateMap<Item, AuctionUpdated>();
+
+        CreateMap<Auction, AuctionDeleted>();
     }
 }
